b88c146ee76cf2d5a34d5a71dd2de324e4818751
Levente Bagi <bagilevi@gmail.com>
2011-08-14 18:44:02 +0100

    ability to set font size

--- wikindle.rb
+++ wikindle.rb
@@ -4,6 +4,11 @@ require 'cgi'
 $: << './lib'
 require 'article'
 
+before do
+  @settings = Settings.new(self)
+  @path = request.path
+end
+
 get '/about' do
   erb :about
 end
@@ -29,6 +34,49 @@ def set_template_params_from_article article
   @smartphone_url = article.smartphone_url
   @mobile_url     = article.mobile_url
 end
+
+get '/settings' do
+  @title = "Settings - wikindle.org"
+  @size_options = Settings::SIZE_OPTIONS
+  @current_size = @settings.size
+  haml :settings, :layout_engine => :erb
+end
+
+post '/settings' do
+  @settings.save_from_input
+  redirect '/settings'
+end
+
+class Settings
+  SIZE_OPTIONS = [12, 14, 16, 18, 20, 24, 28, 32, 36]
+
+  def initialize controller
+    @controller = controller
+  end
+
+  attr_reader :controller
+
+  def cookies
+    controller.request.cookies
+  end
+
+  def size
+    (cookies["size"] || 18).to_i
+  end
+
+  def smaller_size
+    (size * 0.85).round
+  end
+
+  def self.save(controller)
+    new(controller).save
+  end
+
+  def save_from_input
+    controller.response.set_cookie("size", controller.params["size"])
+  end
+end
+
 
 not_found do
   'This is nowhere to be found.'
